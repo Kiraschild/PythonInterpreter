ad41484a0d61bf0bf506a0adc099c2eff3db5fec
Vedant Patel <104895751+Kiraschild@users.noreply.github.com>
2024-11-03 18:04:17 -0500

    Add files via upload

---
+++ Here/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Python Interpreter',
+  description: 'Simulation and Visulaization of Python Interpreter using React/Next.js by 20BCE012, 20BCE024, 20BCE070',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ Here/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.css'
+
+import Header from '@/components/header/header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+
+      </main>
+    </>
+  )
+}
